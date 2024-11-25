97605e277d260ddd0f53a3741e1d142f7eb7ddc7
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-11-25 01:14:58 +0100

    norm

--- maping_sort.c
+++ maping_sort.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 18:30:55 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/11/25 00:41:55 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/11/25 01:13:05 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,45 +38,51 @@ int	find_alternative_move(t_stack *stack_a, t_stack *stack_b)
 	return (1);
 }
 
-int	find_best_move(t_stack *stack_a, t_stack *stack_b)
+int	set_best(t_stack *stack_a, t_stack *stack_b, int b_index, int best_score)
 {
 	t_node	*head_a;
-	t_node	*head_b;
-	int		cost_a;
 	int		cost_b;
-	int		total_cost;
 	int		score;
-	int		best_score;
 	int		c_index;
+
+	cost_b = moves_qty(stack_b, b_index);
+	head_a = stack_a->top;
+	while (head_a)
+	{
+		c_index = stack_a->top->index;
+		if (head_a->next)
+			c_index = head_a->next->index;
+		if (head_a->index < b_index && b_index < c_index)
+		{
+			if (get_cost(moves_qty(stack_a, head_a->index), cost_b) < 3)
+			{
+				score = get_cost(moves_qty(stack_a, head_a->index), cost_b)
+					+ ft_unsigend(head_a->index - c_index);
+				if (score < best_score)
+				{
+					best_score = score;
+					stack_a->best = c_index;
+					stack_b->best = b_index;
+				}
+			}
+		}
+		head_a = head_a->next;
+	}
+	return (best_score);
+}
+
+int	find_best_move(t_stack *stack_a, t_stack *stack_b)
+{
+	t_node	*head_b;
+	int		cost_b;
+	int		best_score;
 
 	head_b = stack_b->top;
 	best_score = 2147483647;
 	while (head_b)
 	{
 		cost_b = moves_qty(stack_b, head_b->index);
-		head_a = stack_a->top;
-		while (head_a)
-		{
-			c_index = stack_a->top->index;
-			if (head_a->next)
-				c_index = head_a->next->index;
-			if (head_a->index < head_b->index && head_b->index < c_index)
-			{
-				cost_a = moves_qty(stack_a, head_a->index);
-				total_cost = get_cost(cost_a, cost_b);
-				if (total_cost < 3)
-				{
-					score = total_cost + ft_unsigend(head_a->index - c_index);
-					if (score < best_score)
-					{
-						best_score = score;
-						stack_a->best = c_index;
-						stack_b->best = head_b->index;
-					}
-				}
-			}
-			head_a = head_a->next;
-		}
+		best_score = set_best(stack_a, stack_b, head_b->index, best_score);
 		head_b = head_b->next;
 	}
 	if (best_score == 2147483647)
