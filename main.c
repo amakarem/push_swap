757b7216a723694010c7ebdeeff376d59bb1eb2c
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-11-25 02:08:37 +0100

    pars input if come as string

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:26 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/11/24 23:45:41 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/11/25 02:07:58 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,37 @@ void	push_swap(t_stack *stack_a, t_stack *stack_b)
 	maping_sort(stack_a, stack_b);
 }
 
-int	main(int argc, char **argv)
+int	parse_input(t_stack *a, int argc, char **argv, int end)
 {
 	int		i;
+
+	i = argc - 1;
+	while (i > end)
+	{
+		if (!ft_isnum(argv[i]) || ft_already_exist_stack(a, ft_atoi(argv[i])))
+		{
+			ft_printf("Error\n");
+			ft_printf("\n%i", argc);
+			return (-1);
+		}
+		push(a, ft_atoi(argv[i]), -1);
+		i--;
+	}
+	return (1);
+}
+
+static int	ft_array_len(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+		i++;
+	return (i);
+}
+
+int	main(int argc, char **argv)
+{
 	t_stack	*a;
 	t_stack	*b;
 
@@ -34,17 +62,15 @@ int	main(int argc, char **argv)
 	b = init_stack();
 	if (!b)
 		return (ft_printf("Error\n"), free_stack(a), -1);
-	i = argc - 1;
-	while (i > 0)
+	if (argc == 2 && !ft_isnum(argv[1]))
 	{
-		if (!ft_isnum(argv[i]) || ft_already_exist_stack(a, ft_atoi(argv[i])))
-		{
-			ft_printf("Error\n");
-			return (free_stack(a), -1);
-		}
-		push(a, ft_atoi(argv[i]), -1);
-		i--;
+		argv = ft_split(argv[1], ' ');
+		argc = ft_array_len(argv);
+		free(argv);
+		if (parse_input(a, argc, argv, -1) == 1)
+			push_swap(a, b);
 	}
-	push_swap(a, b);
+	else if (parse_input(a, argc, argv, 0) == 1)
+		push_swap(a, b);
 	return (free_stack(a), free_stack(b), 0);
 }
